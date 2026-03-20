1894f78fef7412daec716f1e697eecf1542d6526
pedro <santoslimap3@gmail.com>
2026-03-20 12:04:06 +0800

    hello

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.1].define(version: 2026_03_19_033338) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "chats", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "session_id", null: false
@@ -69,7 +72,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_19_033338) do
     t.datetime "created_at", null: false
     t.string "email"
     t.string "encrypted_password", default: "", null: false
-    t.string "password_digest"
     t.datetime "remember_created_at"
     t.datetime "reset_password_sent_at"
     t.string "reset_password_token"
