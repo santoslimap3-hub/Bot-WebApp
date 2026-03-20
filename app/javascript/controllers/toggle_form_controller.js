57bb81d7ff4d1541a0df386831bb9f1eac2f0535
pedro <santoslimap3@gmail.com>
2026-03-20 11:19:28 +0800

    make it better

--- app/javascript/controllers/toggle_form_controller.js
+++ app/javascript/controllers/toggle_form_controller.js
@@ -1,43 +1,53 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-    static targets = ["loginCard", "signupCard", "signInButton", "password", "password_confirmation", "email", "signUpEmail", "signUpPassword", "signUpPasswordConfirmation", "signupButton"]
+    static targets = [
+        "loginCard", "signupCard",
+        "loginTitle", "loginSubtitle", "signupTitle", "signupSubtitle",
+        "signInButton", "password", "email",
+        "signUpEmail", "signUpPassword", "signUpPasswordConfirmation", "signupButton"
+    ]
 
-    connect(){}
+    connect() {}
+
     toggle() {
-
         this.loginCardTarget.classList.toggle("d-none")
         this.signupCardTarget.classList.toggle("d-none")
 
-    }
-    checkFields() {
-
-        if (
-
-            this.signUpPasswordTarget.value === this.signUpPasswordConfirmationTarget.value && 
-            this.signUpPasswordTarget.value.length > 0 && 
-            this.signUpPasswordConfirmationTarget.value.length > 0 && 
-            this.signUpEmailTarget.value.length > 0 && 
-            this.signUpEmailTarget.value.includes("@") &&
-            this.signUpEmailTarget.value.includes(".")
-
-        ) {
-
-            this.signupButtonTarget.disabled = false;
-
-        } else if (
-            this.passwordTarget.value.length > 0 && 
-            this.emailTarget.value.length > 0 && 
-            this.emailTarget.value.includes("@") &&
-            this.emailTarget.value.includes(".")
-        ) {
-            this.signInButtonTarget.disabled = false;
-        } else {
-
-            this.signupButtonTarget.disabled = true;
-            this.signInButtonTarget.disabled = true;
-
+        if (this.hasLoginTitleTarget) {
+            this.loginTitleTarget.classList.toggle("d-none")
+            this.loginSubtitleTarget.classList.toggle("d-none")
+            this.signupTitleTarget.classList.toggle("d-none")
+            this.signupSubtitleTarget.classList.toggle("d-none")
         }
-
     }
-};
+
+    checkFields() {
+        const isSignUpVisible = this.hasSignupCardTarget && !this.signupCardTarget.classList.contains("d-none")
+
+        if (isSignUpVisible) {
+            const email = this.signUpEmailTarget.value
+            const pass = this.signUpPasswordTarget.value
+            const confirm = this.signUpPasswordConfirmationTarget.value
+
+            this.signupButtonTarget.disabled = !(
+                email.length > 0 &&
+                email.includes("@") &&
+                email.includes(".") &&
+                pass.length > 0 &&
+                confirm.length > 0 &&
+                pass === confirm
+            )
+        } else {
+            const email = this.emailTarget.value
+            const pass = this.passwordTarget.value
+
+            this.signInButtonTarget.disabled = !(
+                email.length > 0 &&
+                email.includes("@") &&
+                email.includes(".") &&
+                pass.length > 0
+            )
+        }
+    }
+}
